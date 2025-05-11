d4df5be2147afb70d3fce7f26564ce041bb2a995
keithmccall <keithcodes@gmail.com>
2025-05-10 17:41:18 -0700

    Autocomplete

---
+++ autocompletion/src/input-autocomplete.tsx
@@ -0,0 +1,111 @@
+import "./input-autocomplete.css";
+import { useEffect, useRef, useState, type KeyboardEvent } from "react";
+
+const options = {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json",
+        "X-API-Key":
+            "678a284686a46f0838637f0978824393bf4f630e961e1b2fc470cb84de61c018",
+    },
+};
+
+const defaultSuggestions: unknown[] = [];
+export const InputAutocomplete = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [suggestions, setSuggestions] =
+        useState<unknown[]>(defaultSuggestions);
+    const [activeSuggestion, setActiveSuggestion] = useState(-1);
+    const [cache, setCache] = useState<Record<string, unknown[]>>({});
+    const debounceRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+    console.log(cache);
+    useEffect(() => {
+        if (!inputValue) {
+            setSuggestions(defaultSuggestions);
+            return;
+        }
+        if (cache[inputValue]) {
+            setSuggestions(cache[inputValue]);
+        } else {
+            debounceRef.current = setTimeout(() => {
+                fetch(
+                    `https://api.peopledatalabs.com/v5/autocomplete?field=company&text=${inputValue}&size=10&titlecase=false&pretty=false`,
+                    options,
+                )
+                    .then((res) => res.json())
+                    .then((res) => {
+                        setCache((_cache) => {
+                            return {
+                                ..._cache,
+                                [inputValue]: res.data,
+                            };
+                        });
+                        setSuggestions(res.data);
+                    });
+            }, 300);
+        }
+
+        return () => {
+            if (debounceRef.current !== null) {
+                clearTimeout(debounceRef.current);
+            }
+        };
+    }, [inputValue]);
+
+    const onKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (!suggestions.length || !event) {
+            return;
+        }
+        switch (event.key) {
+            case "ArrowDown":
+                event.preventDefault();
+                setActiveSuggestion((sug) => {
+                    return sug === suggestions.length - 1 ? 0 : sug + 1;
+                });
+                break;
+            case "ArrowUp":
+                event.preventDefault();
+                setActiveSuggestion((sug) => {
+                    return sug === 0 ? suggestions.length - 1 : sug - 1;
+                });
+                break;
+            case "Enter":
+                event.preventDefault();
+                console.log(suggestions[activeSuggestion]);
+                break;
+            case "Escape":
+                event.preventDefault();
+                setActiveSuggestion(-1);
+                break;
+            default:
+                break;
+        }
+    };
+    return (
+        <div className="input-autocomplete">
+            <input
+                type="text"
+                className="text"
+                onKeyDown={onKeyDown}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            {suggestions.length ? (
+                <div className="suggestions-box">
+                    {suggestions.map(({ meta: suggestion }, index) => {
+                        const { display_name, id } = suggestion || {};
+                        return (
+                            <div
+                                key={id}
+                                className={
+                                    index === activeSuggestion ? "active" : ""
+                                }
+                            >
+                                {display_name}
+                            </div>
+                        );
+                    })}
+                </div>
+            ) : null}
+        </div>
+    );
+};
