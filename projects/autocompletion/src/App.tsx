514934480778dbb8049dee19697d2443a7f3d2fe
keithmccall <keithcodes@gmail.com>
2025-06-01 20:14:09 -0700

    Moving autocompletion

---
+++ projects/autocompletion/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import { CardMatching } from './feature/card-matching/card-matching.tsx';
+import { InputAutocomplete } from './feature/input-autocomplete/input-autocomplete.tsx';
+import viteLogo from '/vite.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite x React</h1>
+      <div className="section">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <InputAutocomplete />
+        <CardMatching matchTarget={2} />
+      </div>
+    </>
+  );
+}
+
+export default App;
